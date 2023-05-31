6bca3380b6b820d80da82d5e74c3732d45ba8fb8
Priyanshu Sharma <134008291+Evilgod12priyanshu@users.noreply.github.com>
2023-05-31 19:42:15 +0530

    Add files via upload

---
+++ indexs.js
@@ -0,0 +1,12 @@
+function peep(){
+    console.log("leave house");
+    console.log("move left");
+    console.log("move right");
+    console.log("move left");
+    console.log("move right");
+    console.log("move left");
+    console.log("move centre");
+    console.log("move left");
+    console.log("move right");
+}
+peep();
